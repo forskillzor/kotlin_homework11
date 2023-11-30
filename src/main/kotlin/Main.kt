628eee997f485e5be566194c98dc93406faa7a02
Sergey Orlov <formyfrontend@gmail.com>
2023-11-30 11:25:48 +0700

    imp Stack<T>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,30 @@
+import utils.Stack
+
 fun main() {
     println("Hello World!!!")
+
+    val stack = Stack<Int>()
+
+    println(stack)
+
+    println("Stack is empty: ${stack.isEmpty()}")
+
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    stack.push(4)
+    stack.push(5)
+
+    println(stack)
+
+    println("Stack is empty: ${stack.isEmpty()}")
+
+    val value1 = stack.pop()
+
+    println("$stack, stack pop=$value1")
+
+    val value2 = stack.pop()
+
+    println("$stack, stack pop=$value2")
+
 }
---
+++ src/main/kotlin/utils/Stack.kt
@@ -0,0 +1,22 @@
+package utils
+
+class Stack<T> {
+    val list = mutableListOf<T>()
+    fun push(item: T) {
+        list.add(item)
+    }
+    fun pop(): T {
+        val last = list.lastIndex
+        val result = list[last]
+        list.removeAt(last)
+        return result
+    }
+    fun isEmpty(): Boolean{
+        return list.size == 0
+    }
+
+    override fun toString(): String {
+        return "Stack(list=$list)"
+    }
+
+}
