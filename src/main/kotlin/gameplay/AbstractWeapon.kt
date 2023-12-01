e0f87556f02471a33ed46666c4fb274bef57ab54
Sergey Orlov <skillzorwispa@gmail.com>
2023-12-02 06:33:14 +0700

    imp AbstractWeapon.kt

--- src/main/kotlin/gameplay/AbstractWeapon.kt
+++ src/main/kotlin/gameplay/AbstractWeapon.kt
@@ -2,14 +2,36 @@ package gameplay
 
 import utils.Stack
 
-abstract class AbstractWeapon {
-    abstract val magazineSize: Int
-    abstract val fireType: FireType
-    abstract val magazin: Stack<Ammo>
+// TODO change abstract class to open class
+
+class AbstractWeapon {
+    val ammoType: Ammo = Ammo.BERETTA
+    val fireType: FireType = FireType.singleShoot
+    protected val magazineSize: Int = 10
+    protected var magazin: Stack<Ammo> = Stack()
+
     val isEmptyMagazin: Boolean
         get() = magazin.size == 0
 
-    abstract fun createBullet(): Ammo
-    abstract fun getBulletForShoot()
-    abstract fun reload()
+    fun createBullet(): Ammo {
+        return when (ammoType) {
+            Ammo.BERETTA -> Ammo.BERETTA
+            Ammo.UPS -> Ammo.UPS
+            Ammo.MAKAROV -> Ammo.MAKAROV
+            Ammo.MACHINEGUN -> Ammo.MACHINEGUN
+            Ammo.AK47 -> Ammo.AK47
+            Ammo.M16 -> Ammo.M16
+        }
+    }
+
+    fun getBulletForShoot(): Ammo {
+        return magazin.pop()
+    }
+    fun reload() {
+        val newMagazin = Stack<Ammo>()
+        for (ammo in 0 until magazineSize) {
+            newMagazin.push(createBullet())
+        }
+        magazin = newMagazin
+    }
 }
