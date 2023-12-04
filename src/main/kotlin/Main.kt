da7ea2a1677d7bea9c4c58dbbb5866b4dee4f695
Sergey Orlov <skillzorwispa@gmail.com>
2023-12-05 06:12:32 +0700

    begining Battle.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,16 @@
+import gameplay.Battle
 import gameplay.Team
-import warriors.Captain
-import warriors.General
-import warriors.Soldier
 
 fun main() {
-//    val soldier1 = Soldier("Антон")
-//    val soldier2 = Soldier("Сергей")
-//
-//    val captain1 = Captain("Валера")
-//    val captain2 = Captain("Геннадий")
-//
-//    val general1 = General("Потапов")
-//    val general2 = General("Стрелков")
-//
-//    soldier1.attack(soldier2)
-//    soldier1.attack(captain1)
-//    captain1.attack(soldier1)
-//    general1.attack(soldier1)
 
-    val Assault = Team("Assaults",20)
-    val Terrorist = Team("Terrorists", 20)
+    val assault = Team("Assaults",20)
+    val terrorist = Team("Terrorists", 20)
+
+    val battle = Battle(assault, terrorist)
+
+    while (!battle.isFinished) {
+        battle.getState()
+        battle.next()
+    }
 
 }
---
+++ src/main/kotlin/gameplay/Battle.kt
@@ -0,0 +1,23 @@
+package gameplay
+
+class Battle (
+    val team1: Team,
+    val team2: Team
+        ){
+
+    var isFinished = false
+
+    fun getState() {
+        /* TODO get victory criterias
+        Here is criterias
+        Here is battle state
+         */
+    }
+
+    fun next() {
+        /* TODO(next iteration of battle)
+        Here is rules (one to one or another rules)
+        Here is itaretion
+         */
+    }
+}
